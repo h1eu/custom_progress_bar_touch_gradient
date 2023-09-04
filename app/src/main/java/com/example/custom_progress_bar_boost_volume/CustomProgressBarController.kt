bb87fda1e8ed35d391fefd1b33250bdacf3afce5
h1eu <dev.hieult@gmail.com>
2023-09-04 23:27:54 +0700

    add

--- app/src/main/java/com/example/custom_progress_bar_boost_volume/CustomProgressBarController.kt
+++ app/src/main/java/com/example/custom_progress_bar_boost_volume/CustomProgressBarController.kt
@@ -1,15 +1,20 @@
 package com.example.custom_progress_bar_boost_volume
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import androidx.core.content.ContextCompat
 
 class CustomProgressBarController(context: Context,attr: AttributeSet): View(context,attr) {
+    private var startX: Float? = null
+    private var offSetX: Float? = null
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val width = MeasureSpec.makeMeasureSpec(widthMeasureSpec, MeasureSpec.EXACTLY)
@@ -29,6 +34,32 @@ class CustomProgressBarController(context: Context,attr: AttributeSet): View(con
                 null
             )
         }
+    }
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val temporaryX = event?.x
+        when(event?.action){
+            MotionEvent.ACTION_DOWN -> {
+                startX = temporaryX
+                offSetX = 0f
+                Log.e("motion", "onTouchEvent: cham xuong", )
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val dX = temporaryX?.minus(startX!!)
+                dX?.let { offsetLeftAndRight(it.toInt()) }
+
+                if (dX != null) {
+                    offSetX = offSetX?.plus(dX)
+                }
+
+
+                startX = temporaryX
+                Log.e("motion", "onTouchEvent: sau khi di chuyen", )
+            }
+        }
+
+
+        return true
     }
 
     private fun drawableToBitmap(drawable: Drawable): Bitmap? {
